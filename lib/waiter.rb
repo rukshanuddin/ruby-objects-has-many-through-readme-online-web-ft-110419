a95e37a9923a210841ceaa50bff76f01f02c9c16
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-18 23:41:20 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,20 @@
 class Waiter
 
+
+    attr_accessor :experience
+    attr_reader :name
+
+    @@all = []
+
+    def initialize(name, years)
+      @name = name
+      @experience = years
+      @@all << self
+    end
+
+    def self.all
+      @@all
+    end
+
+
 end
