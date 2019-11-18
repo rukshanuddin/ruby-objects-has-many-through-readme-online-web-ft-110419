bcb4cd91e3e9b41c40e501426f562e1ba39d9376
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-18 23:43:47 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -2,10 +2,13 @@ class Meal
   attr_accessor
   attr_reader
 
+  @@all = []
+
   def initialize(waiter, customer, total, tip)
     @waiter = waiter
     @customer = customer
     @total = total
     @tip = tip
+    @@all << self
   end
 end
